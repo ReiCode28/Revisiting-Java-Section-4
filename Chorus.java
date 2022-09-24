053981ce31e7999de4309f867db09c561ef82ea0
randy <randyr_1993@yahoo.com>
2022-09-24 01:24:00 -0400

    Fixed gitignore

---
+++ Chorus.java
@@ -0,0 +1,22 @@
+public class Chorus{
+    public static void main(String[] args) {
+        singChorus();
+        singChorus();
+        singChorus();
+        singChorus();
+    }
+    public static void singChorus(){
+        System.out.println("Don't blame it on the sunshine");
+        System.out.println("Don't blame it on the moonlight");
+        System.out.println("Don't blame it on the good times");
+        System.out.println("Blame it on the boogie\n");
+    }
+
+}
+
+
+// Function:
+// A grouping of code that performs a task
+// It makes code more organized and reusable
+// Copy and pasting code is bad practice. Write your task in a function instead and then call the function as many times as you need to.
+// function and method mean the same thing 
