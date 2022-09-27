463b0e442d549c0c234da70e438d4037b5ee2623
randy <randyr_1993@yahoo.com>
2022-09-26 23:14:02 -0400

    Completed BuiltInFunctions

---
+++ BuiltInFunctions.java
@@ -0,0 +1,21 @@
+public class BuiltInFunctions{
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+
+        double sine = Math.sin(1.2);
+        System.out.println("The sin of 1.2: " + sine);
+
+        double power = Math.pow(2, 4);
+        System.out.println("2 to the power of 4 is: " + power);
+
+        double random = Math.random() * 10; 
+        System.out.println("The random value is: " + random);
+    }
+}
+/*
+ * There are THOUSANDS of built in code.
+ * But a good developer doesn't memorize code.
+ * A good developer uses the internet:
+ * 1. to read documentation
+ * 2. and find resources
+ */
